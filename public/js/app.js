592468a197c1d97ab2d4e0ef2b699bef699f91ec
mogetutu <imogetutu@gmail.com>
2013-04-04 02:43:02 +0300

    Few Comments on app.js

--- public/js/app.js
+++ public/js/app.js
@@ -12,19 +12,24 @@ App.IndexRoute = Ember.Route.extend({
     this.transitionTo('posts');
   }
 });
-
+// Posts Controller
 App.PostsRoute = Ember.Route.extend({
     model: function() {
         return App.Post.find();
     }
 });
 
+// Post Model
 App.Post = DS.Model.extend({
     title : DS.attr('string'),
     body : DS.attr('string')
 });
 
+
+// RESTAdapter using ember-data.js <-- ensure dependancy is included after ember.js -->
 App.Adapter = DS.RESTAdapter.extend({
+  // do host and endpoint customisations here
+  // Test Example
   url: "http://ember.local"
 });
 
